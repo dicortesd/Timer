8ee477ee98af435ce479f85485fed658daec5b81
zab <thomas.zabera@ensta-bretagne.org>
2023-03-23 16:16:59 -0500

    creacion proyecto

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <CreateProject msg="Test de site"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import CreateProject from '@/components/CreateProject.vue'
+
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    CreateProject,
   },
 });
 </script>
