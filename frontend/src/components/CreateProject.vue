05f6794fb9dd8da32e6c3cd07e4a4c1fa268a1d0
zab <thomas.zabera@ensta-bretagne.org>
2023-03-23 18:25:20 -0500

    espanol

--- frontend/src/components/CreateProject.vue
+++ frontend/src/components/CreateProject.vue
@@ -1,48 +1,63 @@
 <template>
-    <div>
-      <h1>Créer un nouveau projet</h1>
-      <form @submit.prevent="createProject">
-        <label for="projectName">Nom du projet:</label>
-        <input type="text" id="projectName" v-model="projectName">
-        <br>
-        <label for="projectDescription">Description:</label>
-        <textarea id="projectDescription" v-model="projectDescription"></textarea>
-        <br>
-        <label for="projectAssignee">Assigner à:</label>
-        <select id="projectAssignee" v-model="projectAssignee">
-          <option value="">-- Sélectionner une personne --</option>
-          <option v-for="person in people" :key="person.id" :value="person.id">{{ person.name }}</option>
-        </select>
-        <br>
-        <button type="submit">Créer</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        projectName: '',
-        projectDescription: '',
-        projectAssignee: '',
-        people: [
-          { id: 1, name: 'John' },
-          { id: 2, name: 'Jane' },
-          { id: 3, name: 'Bob' }
-        ]
-      }
-    },
-    methods: {
-      createProject() {
-        // Envoyer les données du formulaire au backend pour créer un nouveau projet
-        console.log('Créer un nouveau projet:', this.projectName, this.projectDescription, this.projectAssignee);
-        // Réinitialiser le formulaire
-        this.projectName = '';
-        this.projectDescription = '';
-        this.projectAssignee = '';
-      }
+  <div>
+    <h1>Crear un nuevo proyecto</h1>
+    <form @submit.prevent="createProject">
+      <label for="projectName">Nombre del proyecto:</label>
+      <input type="text" id="projectName" v-model="projectName">
+      <br>
+      <label for="projectDescription">Descripción:</label>
+      <textarea id="projectDescription" v-model="projectDescription"></textarea>
+      <br>
+      <label for="projectAssignee">Asignar a:</label>
+      <select id="projectAssignee" v-model="projectAssignee">
+        <option value="">-- Seleccionar una persona --</option>
+        <option v-for="person in people" :key="person.id" :value="person.id">{{ person.name }}</option>
+      </select>
+      <br>
+      <label for="projectDueDate">Fecha de entrega:</label>
+      <select id="projectDueDate" v-model="projectDueDate">
+        <option value="">-- Seleccionar una fecha --</option>
+        <option v-for="date in dueDates" :key="date">{{ date }}</option>
+      </select>
+      <br>
+      <button type="submit">Crear</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projectName: '',
+      projectDescription: '',
+      projectAssignee: '',
+      projectDueDate: '',
+      people: [
+        { id: 1, name: 'Juan' },
+        { id: 2, name: 'Maria' },
+        { id: 3, name: 'Pedro' }
+      ],
+      dueDates: [
+        '1 semana',
+        '2 semanas',
+        '3 semanas',
+        '1 mes',
+        '2 meses',
+        '3 meses'
+      ]
+    }
+  },
+  methods: {
+    createProject() {
+      // Enviar los datos del formulario al backend para crear un nuevo proyecto
+      console.log('Crear un nuevo proyecto:', this.projectName, this.projectDescription, this.projectAssignee, this.projectDueDate);
+      // Reinicializar el formulario
+      this.projectName = '';
+      this.projectDescription = '';
+      this.projectAssignee = '';
+      this.projectDueDate = '';
     }
   }
-  </script>
-  
+}
+</script>
