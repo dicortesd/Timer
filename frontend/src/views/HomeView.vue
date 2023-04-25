db05371d9f91d097f7e927d3b0b127af4b22e1f7
Virazou <virazelsmarie@orange.fr>
2023-04-26 01:55:53 +0200

    resume tareas de un proyecto

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" style="width: 200px;">
-    <HomeComponent msg="Test de site"/>
+    <ResumeProyecto msg="Test de site"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HomeComponent from '@/components/HomeComponent.vue';
-import CreateProject from '@/components/CreateProject.vue'
-import SeeClient from '@/components/SeeClient.vue'
-import SeeProyecto from '@/components/SeeProyecto.vue'
+import CreateProject from '@/components/CreateProject.vue';
+import SeeClient from '@/components/SeeClient.vue';
+import SeeProyecto from '@/components/SeeProyecto.vue';
+import ResumeProyecto from '@/components/ResumeProyecto.vue';
 import LoginComponent from '@/components/LoginCompo.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HomeComponent,
+    ResumeProyecto,
   },
 });
 </script>
