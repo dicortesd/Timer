c55eadc9da4fb9ba28d05cd7ff8fefee7f988817
Virazou <virazelsmarie@orange.fr>
2023-05-05 01:26:29 +0200

    chrono

---
+++ frontend/src/components/TaskUser.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <h2>Tasks</h2>
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+          {{ task.name }} - {{ task.time }}<br>
+          <time-counter :task="task"></time-counter>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import TimeCounter from "./TimeCounter.vue";
+  
+  export default {
+    components: {
+      TimeCounter,
+    },
+    data() {
+      return {
+        tasks: [
+          {
+            name: "Task 1",
+            time: 0,
+          },
+          {
+            name: "Task 2",
+            time: 0,
+          },
+        ],
+      };
+    },
+  };
+  </script>
+  
