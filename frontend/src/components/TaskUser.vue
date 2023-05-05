c27ecf96e5de21ff3ea9408ef3b4909abf454807
Virazou <virazelsmarie@orange.fr>
2023-05-05 02:08:45 +0200

    Chrono para tarea

--- frontend/src/components/TaskUser.vue
+++ frontend/src/components/TaskUser.vue
@@ -1,36 +1,92 @@
 <template>
-    <div>
-      <h2>Tasks</h2>
-      <ul>
-        <li v-for="(task, index) in tasks" :key="index">
-          {{ task.name }} - {{ task.time }}<br>
-          <time-counter :task="task"></time-counter>
-        </li>
-      </ul>
+  <div class="task-table">
+    <h2>Tasks</h2>
+    <div class="task-container">
+      <div v-for="(task, index) in tasks" :key="index" class="task-square">
+        <div class="task-header">
+          <span class="task-name">{{ task.name }}</span>
+          <time-counter :task="task" :key="task.name" @update:task="updateTask" class="task-time-counter"></time-counter>
+        </div>
+        <div class="task-body">
+          {{ task.time }}
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import TimeCounter from "./TimeCounter.vue";
-  
-  export default {
-    components: {
-      TimeCounter,
+  </div>
+</template>
+
+<script>
+import TimeCounter from "./TimeCounter.vue";
+
+export default {
+  components: {
+    TimeCounter,
+  },
+  data() {
+    return {
+      tasks: [
+        { name: "Task 1", time: 0 },
+        { name: "Task 2", time: 0 },
+        { name: "Task 3", time: 0 },
+      ],
+    };
+  },
+  methods: {
+    updateTask(updatedTask) {
+      let taskIndex = this.tasks.findIndex((task) => task.name === updatedTask.name);
+      this.tasks[taskIndex].time = updatedTask.time;
     },
-    data() {
-      return {
-        tasks: [
-          {
-            name: "Task 1",
-            time: 0,
-          },
-          {
-            name: "Task 2",
-            time: 0,
-          },
-        ],
-      };
-    },
-  };
-  </script>
-  
+  },
+};
+</script>
+
+<style scoped>
+.task-table {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.task-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.task-square {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 250px;
+  height: 200px;
+  margin: 20px;
+  padding: 10px;
+  border-radius: 20px;
+  background-color: #f1f1f1;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+}
+
+.task-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+.task-name {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.task-time-counter {
+  font-size: 1.2rem;
+}
+
+.task-body {
+  font-size: 1.2rem;
+  text-align: center;
+  color: #666;
+}
+</style>
---
+++ frontend/src/components/TimeCounter.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h3>{{ task.name }}</h3>
+    <div>Time: {{ timeString }}</div>
+    <button v-if="!timerRunning" @click="startTimer">Start</button>
+    <button v-if="timerRunning" @click="stopTimer">Stop</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      timerRunning: false,
+      startTime: 0,
+      elapsedTime: 0,
+      timerInterval: null,
+    };
+  },
+  computed: {
+    timeString() {
+    const totalSeconds = Math.floor((this.task.time + this.elapsedTime) / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+    return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+},
+
+  },
+  methods: {
+    startTimer() {
+      this.timerRunning = true;
+      this.startTime = Date.now();
+      this.timerInterval = setInterval(this.updateTime, 1000);
+    },
+    stopTimer() {
+      this.timerRunning = false;
+      clearInterval(this.timerInterval);
+      const updatedTask = { ...this.task, time: this.task.time + this.elapsedTime };
+      this.$emit("update:task", updatedTask);
+    },
+    updateTime() {
+      this.elapsedTime = Date.now() - this.startTime;
+    },
+  },
+};
+</script>
